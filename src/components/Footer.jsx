6c4b971505c316f40ee708b5f6999208ecfa1561
Aman verman <83340249+aman567441@users.noreply.github.com>
2025-04-24 13:51:20 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -19,8 +19,8 @@ const Footer = () => {
         <a href="https://vercel.com" target="_blank" rel="noopener noreferrer"><SiVercel /></a>
           <a href="https://app.netlify.com/teams/aman567441/sites" target="_blank" rel="noopener noreferrer"><SiNetlify /></a>
           <a href="https://wa.me/9262539269" target="_blank" rel="noopener noreferrer"><FaWhatsapp /></a>
-         <a href="https://github.com/your-username" target="_blank" rel="noopener noreferrer"><FaGithub /></a>
-         <a href="https://www.linkedin.com/in/your-profile" target="_blank" rel="noopener noreferrer"><FaLinkedin /></a>
+         <a href="https://github.com/aman567441" target="_blank" rel="noopener noreferrer"><FaGithub /></a>
+         <a href="https://www.linkedin.com/in/aman-kumar-b706b6202/" target="_blank" rel="noopener noreferrer"><FaLinkedin /></a>
         <a href="https://www.naukri.com/mnjuser/profile" target="_blank" rel="noopener noreferrer"><FaBriefcase /></a>
         <a href="https://www.instagram.com/your-profile" target="_blank" rel="noopener noreferrer"><FaInstagram /></a>
         </div>
